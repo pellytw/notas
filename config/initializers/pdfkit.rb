981c0f5323e4a484b1e85491ebc2e472a13f4dd1
Pregase Server <mde@chubut.edu.ar>
2014-05-06 10:50:41 -0300

    cambios del pdfkit produccion

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,5 +1,5 @@
 PDFKit.configure do |config|
-  config.wkhtmltopdf =   Rails.root.join('extras', 'wkhtmltopdf').to_s
+  config.wkhtmltopdf =   Rails.root.join('extras', 'wkhtmltopdf-amd64').to_s
   config.default_options = {
     :page_size => 'A4',
     :print_media_type => true
